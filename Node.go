03131da7917ae2d5fc30d4e2225c4de4cc0a48aa
Manfred Dreese <md@dreese.de>
2018-02-04 21:40:32 +0100

    add: Improved Search functionality

--- Node.go
+++ Node.go
@@ -16,32 +16,80 @@ type Node struct {
 	Data     interface{}
 }
 
-func (node *Node) AddChild(id uuid.UUID, label string, data interface{}) *Node {
+// NodeCriteria allows to specify a criteria for matching nodes in search queries
+type NodeCriteria func(subject *Node) (found bool)
 
-	newNode := &Node{
-		Id:     id,
-		Label:  label,
-		Parent: node,
-		Data:   data,
+// AddChild adds a child to the current node and returns a reference to the child
+func (node *Node) AddChild(id uuid.UUID, label string, data interface{}) *Node {
+	newNode := node.Tree.NewNode(id, label, data)
+	newNode.Parent = node
+	node.Children = append(node.Children, newNode)
+	return newNode
+}
+
+// AddChildren adds n children to the current node and returns a reference to it
+func (node *Node) AddChildren(nodes ...*Node) (self *Node) {
+	for _, newNode := range nodes {
+		node.Parent = node
+		node.Children = append(node.Children, newNode)
+	}
+	return node
+}
+
+// GetChildren returns the child node of a given node matching a criteria
+func (node *Node) GetChildren(criteria NodeCriteria, maxMatches int) (results []*Node, numOfMatches int) {
+	for _, newNode := range node.Children {
+		if criteria(newNode) == true {
+			results = append(results, newNode)
+			numOfMatches++
+			if maxMatches > 0 && numOfMatches == maxMatches {
+				return
+			}
+		}
+	}
+	return
+}
+
+// GetChild returns the first child node of a given node matching a criteria
+func (node *Node) GetChild(criteria NodeCriteria) (result *Node, found bool) {
+	if matches, n := node.GetChildren(criteria, 1); n > 0 {
+		result, found = matches[0], true
+	}
+	return
+}
+
+// GetChildByLabel returns a reference to a child of a certain Node with the title specified
+func (node *Node) GetChildByLabel(label string) (needle *Node, found bool) {
+	return node.GetChild(func(subject *Node) bool {
+		return subject.Label == label
+	})
+}
+
+// GetChildByLabel returns a reference to a child of a certain Node with the title specified
+func (node *Node) GetChildByUuid(uuid uuid.UUID) (needle *Node, found bool) {
+	return node.GetChild(func(subject *Node) bool {
+		return subject.Id == uuid
+	})
 	}
 	node.Children = append(node.Children, newNode)
 
 	return newNode
 }
 
-func (node *Node) FindByPath(path string) (needle Node, exists bool) {
-	exists = false
+// FindByPath returns a reference to a child node relative to the current node expressed by a "child1/child2/childn" reference
+func (node *Node) FindByPath(path string) (needle *Node, exists bool) {
 	tokens := strings.Split(path, "/")[1:]
+	return node.FindByPathTokens(tokens)
+}
 
-	for _, child := range node.Children {
-		if child.Label == tokens[0] {
-			if len(tokens) == 1 {
-				needle = *child
-				exists = true
-				return
+// FindByPathTokens returns a reference to a child node relative to the current node expressed by a slice of child node titles
+func (node *Node) FindByPathTokens(tokens []string) (needle *Node, exists bool) {
+	exists = false
+	if next, found := node.GetChildByLabel(tokens[0]); found {
+		if len(tokens) == 2 {
+			needle, exists = next.GetChildByLabel(tokens[1])
 			} else {
-				return child.FindByPath(strings.Join(tokens, "/"))
-			}
+			return next.FindByPathTokens(tokens[1:])
 		}
 	}
 	return
--- Tree_test.go
+++ Tree_test.go
@@ -1,6 +1,8 @@
 package gotree
 
-import ("testing")
+import (
+	"testing"
+)
 import "github.com/google/uuid"
 
 func TestCreate(t *testing.T) {
@@ -13,11 +15,105 @@ func TestFindByPath(t *testing.T) {
 
 	tree.Root.AddChild(uuid.New(), "robots", nil).
 	AddChild(uuid.New(), "marvin", nil).
-	AddChild(uuid.New(), "quotes", []string {"Robot1"})
+		AddChild(uuid.New(), "quotes", []string{"Robot1"})
 
 	node, found := tree.FindByPath("/robots/marvin/quotes")
 
-	if !found || node.Data.([]string)[0]!="Robot1" {
+	if !found || node.Data.([]string)[0] != "Robot1" {
+		t.Fail()
+	}
+}
+
+func TestFindByPathTokens(t *testing.T) {
+	tree := NewTree()
+
+	tree.Root.AddChild(uuid.New(), "robots", nil).
+		AddChild(uuid.New(), "marvin", nil).
+		AddChild(uuid.New(), "quotes", "MARVIN")
+
+	needle, found := tree.Root.FindByPathTokens([]string{"robots", "marvin", "quotes"})
+
+	if !found || needle.Data.(string) != "MARVIN" {
+		t.Fail()
+	} else {
+		t.Logf("found=%v", needle)
+	}
+}
+
+func TestFindChildByLabelWithExistingChild(t *testing.T) {
+	tree := NewTree()
+
+	tree.Root.AddChild(uuid.New(), "robots", nil).
+		AddChild(uuid.New(), "marvin", nil).
+		AddChild(uuid.New(), "quotes", []string{"Robot1"})
+
+	if needle, exists := tree.Root.GetChildByLabel("robots"); exists {
+		t.Logf("found=%v", needle)
+	} else {
+		t.Fail()
+	}
+}
+
+func TestFindChildByUuidlWithExistingChild(t *testing.T) {
+	tree := NewTree()
+	existingId := uuid.New()
+	tree.Root.AddChild(existingId, "robots", nil)
+
+	if needle, exists := tree.Root.GetChildByUuid(existingId); exists {
+		t.Logf("found=%v", needle)
+	} else {
+		t.Fail()
+	}
+}
+
+func TestFindChildByUuidlWithNonExistingChild(t *testing.T) {
+	tree := NewTree()
+	existingId := uuid.New()
+	tree.Root.AddChild(existingId, "robots", nil)
+
+	if _, exists := tree.Root.GetChildByUuid(uuid.New()); !exists {
+		t.Logf("not found as expected")
+	} else {
+		t.Fail()
+	}
+}
+
+func TestInsertAtPathCreateNewNodeInEmptyTree(t *testing.T) {
+	tree := NewTree()
+
+	if error := tree.Root.InsertAtPath(
+		[]string{"Machine", "Heating", "Zones", "Zone1"},
+		[]*Node{
+			tree.NewNode(uuid.New(), "SetTemperature", nil),
+			tree.NewNode(uuid.New(), "ActualTemperature", nil),
+		}); error != nil {
+		t.Fail()
+	}
+
+	if error := tree.Root.InsertAtPath(
+		[]string{"Machine", "Heating", "Zones", "Zone2"},
+		[]*Node{
+			tree.NewNode(uuid.New(), "SetTemperature", nil),
+			tree.NewNode(uuid.New(), "ActualTemperature", nil),
+		}); error != nil {
+		t.Fail()
+	}
+
+	if _, exists := tree.FindByPath("/Machine/Heating/Zones/Zone2/SetTemperature"); !exists {
+		t.Fatalf("Expected node not found!")
+	}
+
+	if _, exists := tree.FindByPath("/Machine/Heating/Zones"); !exists {
+		t.Fatalf("Node on the way not found!")
+	}
+
+	t.Logf("tree=%v", tree.String())
+}
+
+func TestFindByPathShouldReturnFalseOnEmptyTree(t *testing.T) {
+	tree := NewTree()
+
+	if _, found := tree.FindByPath("/robots/marvin/quotes"); found {
 		t.Fail()
 	}
 
